78584be1695ad0d9ac772f394ba8a59535085160
Sidd Phatak <sphatak@ucvts.org>
2021-06-15 23:26:42 -0400

    final commit forilla

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,11 +1,5 @@
 public class Simulator {
     public static void main(String[] args){
-        for(int x = 0; x < 2; x++){
-            for(int y = 0; y < 2; y ++){
-                OR or = new OR(x, y);
-                System.out.println("Input a: " + x + " Input b: " + y + " Output: " + or.output());
-            }
-        }
         for(int i = 1; i < 3; i++){
             for(int j = 0; j < 2; j++){
                 for(int k = 0; k < 2; k++){
